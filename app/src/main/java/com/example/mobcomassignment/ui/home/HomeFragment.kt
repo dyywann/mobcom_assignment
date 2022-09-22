6745855b6cce889941cefabc581c45988f5ee254
dyywann <88956400+dyywann@users.noreply.github.com>
2022-09-22 10:24:37 +0700

    Initial commit

---
+++ app/src/main/java/com/example/mobcomassignment/ui/home/HomeFragment.kt
@@ -0,0 +1,49 @@
+package com.example.mobcomassignment.ui.home
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.mobcomassignment.MainActivity2
+import com.example.mobcomassignment.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val homeViewModel =
+            ViewModelProvider(this).get(HomeViewModel::class.java)
+
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        binding.btnLinked.setOnClickListener(
+            {
+                val tombol_intent = Intent(this.context, MainActivity2::class.java)
+                startActivity(tombol_intent)
+            }
+        )
+
+
+        val root: View = binding.root
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
